2b1534b67300a3148f196af6b6a2b5b4300dcf4d
Saileshwar Bogam <saileshwar.bogam@zemosolabs.com>
2025-01-10 12:35:20 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+'''
+	Copyright (c) 2022 Skyflow, Inc.
+'''
+from setuptools import setup, find_packages
+import sys
+
+
+if sys.version_info < (3, 8):
+    raise RuntimeError("skyflow requires Python 3.8+")
+current_version = '1.15.1.dev0+9eff324'
+
+setup(
+    name='skyflow',
+    version=current_version,
+    author='Skyflow',
+    author_email='service-ops@skyflow.com',
+    packages=find_packages(where='.', exclude=['test*']),
+    url='https://github.com/skyflowapi/skyflow-python/',
+    license='LICENSE',
+    description='Skyflow SDK for the Python programming language',
+    long_description=open('README.rst').read(),
+    install_requires=[
+    'python_dateutil >= 2.5.3',
+    'setuptools >= 21.0.0',
+    'urllib3 >= 1.25.3, < 2.1.0',
+    'pydantic >= 2',
+    'typing-extensions >= 4.7.1',
+    'DateTime~=5.5',
+    'PyJWT~=2.9.0',
+    'requests~=2.32.3',
+    'coverage',
+    'cryptography',
+    'python-dotenv~=1.0.1'
+],
+    python_requires=">=3.8"
+)
