90ee56c36bf0323f86e68f7eda56832effb72ded
saileshwarbogam <saileshwarbogam@users.noreply.github.com>
2025-01-16 08:46:52 +0000

    Update version for tag 2.0.0b67

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 if sys.version_info < (3, 8):
     raise RuntimeError("requires Python 3.8+")
-current_version = '2.0.0b51'
+current_version = '2.0.0b67'
 
 setup(
     name='hellosailb',
